a9b0a118659a046dc632067b37e4e64070dae177
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2023-12-20 22:53:25 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,30 @@
 /*
  * Adds two given integers and prints the result in a fancy way.
  */
+//import java.util.Scanner;
+/*
+ * Adds two given integers and prints the result in a fancy way.
+ */
 public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+//try (// Put your code here	
+// Scanner Keyboard = new Scanner(System.in)) {
+   //System.out.println("enter a number");
+                  
+                // System.out.println("enter another number");  
+   
+
+	 int x = Integer.parseInt(args[0]);
+	
+        int y = Integer.parseInt(args[1]);
+	
+        int z = x + y;
+        System.out.println(x + " + " + y + " = " + z);
+	
 }
+	}
+
